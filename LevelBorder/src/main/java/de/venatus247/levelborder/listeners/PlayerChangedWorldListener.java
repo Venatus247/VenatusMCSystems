8d8dd9e355bce0c1510f087512ae3560d7f56274
Alexander Wrede <alexander.wrede@venatus247.de>
2021-11-23 18:42:56 +0100

    Fix config var not saved

--- LevelBorder/src/main/java/de/venatus247/levelborder/listeners/PlayerChangedWorldListener.java
+++ LevelBorder/src/main/java/de/venatus247/levelborder/listeners/PlayerChangedWorldListener.java
@@ -1,6 +1,7 @@
 package de.venatus247.levelborder.listeners;
 
 import de.venatus247.levelborder.LevelBorder;
+import de.venatus247.vutils.Logger;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,10 +12,15 @@ public class PlayerChangedWorldListener implements Listener {
     @EventHandler
     public void onWorldChange(PlayerChangedWorldEvent event) {
         Player player = event.getPlayer();
-        if((event.getPlayer().getWorld().getName().equals(LevelBorder.getInstance().getBorderConfig().getNetherWorldName()) && !LevelBorder.getInstance().getBorderConfig().isNetherEntered())
-                || (event.getPlayer().getWorld().getName().equals(LevelBorder.getInstance().getBorderConfig().getEndWorldName()) && !LevelBorder.getInstance().getBorderConfig().isEndEntered())
-        ) {
+        if(event.getPlayer().getWorld().getName().equals(LevelBorder.getInstance().getBorderConfig().getNetherWorldName())
+                && !LevelBorder.getInstance().getBorderConfig().isNetherEntered()) {
             LevelBorder.getInstance().setWorldSpawnLocation(player.getWorld(), player.getLocation());
+            LevelBorder.getInstance().getBorderConfig().setNetherEntered(true);
+            Logger.getInstance().log("Setting Nether border center");
+        } else if(event.getPlayer().getWorld().getName().equals(LevelBorder.getInstance().getBorderConfig().getEndWorldName())
+                && !LevelBorder.getInstance().getBorderConfig().isEndEntered()) {
+            LevelBorder.getInstance().setWorldSpawnLocation(player.getWorld(), player.getLocation());
+            LevelBorder.getInstance().getBorderConfig().setEndEntered(true);
         }
         LevelBorder.getInstance().setPlayerBorder(player);
     }
